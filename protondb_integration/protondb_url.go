d05569d03d33118a61c49812ea64e1680893b504
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-12-12 21:59:10 -0800

    Moving over protondb_integration from steam_integration

---
+++ protondb_integration/protondb_url.go
@@ -0,0 +1,14 @@
+package steam_integration
+
+import (
+	"net/url"
+	"strconv"
+)
+
+func ProtonDBUrl(steamAppId uint32) *url.URL {
+	return &url.URL{
+		Scheme: httpsScheme,
+		Host:   protonDBHost,
+		Path:   appPath + strconv.Itoa(int(steamAppId)),
+	}
+}
---
+++ protondb_integration/url_const.go
@@ -0,0 +1,11 @@
+package steam_integration
+
+// scheme
+const (
+	httpsScheme = "https"
+)
+
+const (
+	protonDBHost = "www.protondb.com"
+	appPath      = "/app/"
+)
