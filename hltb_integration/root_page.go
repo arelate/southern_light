e38511e00d40cf839dfbc89f59436d7122297428
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-12-13 21:19:04 -0800

    Adding HLTB next build getter

--- hltb_integration/root_page.go
+++ hltb_integration/root_page.go
@@ -1,6 +1,11 @@
 package hltb_integration
 
-import "golang.org/x/net/html"
+import (
+	"github.com/boggydigital/match_node"
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+	"strings"
+)
 
 type RootPage struct {
 	Doc *html.Node
@@ -11,5 +16,37 @@ type NextBuildGetter interface {
 }
 
 func (rp *RootPage) GetNextBuild() string {
+	if rp.Doc == nil {
+		return ""
+	}
+
+	if ndsm := match_node.Match(rp.Doc, &nextDataScriptMatcher{}); ndsm != nil && ndsm.FirstChild != nil {
+		for _, line := range strings.Split(ndsm.FirstChild.Data, "\n") {
+			if strings.Contains(line, "buildId") {
+				if parts := strings.Split(line, "\""); len(parts) > 3 {
+					return parts[len(parts)-2]
+				}
+			}
+		}
+	}
+
 	return ""
+}
+
+type nextDataScriptMatcher struct {
+}
+
+func (ndsm *nextDataScriptMatcher) Match(node *html.Node) bool {
+	if node.DataAtom == atom.Script &&
+		len(node.Attr) > 0 {
+
+		for _, attr := range node.Attr {
+			if attr.Key == "id" &&
+				attr.Val == "__NEXT_DATA__" {
+				return true
+			}
+		}
+
+	}
+	return false
 }
