d73cf250e8569c4801702a3137a8c1a180b42d6f
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-12-16 18:22:46 -0800

    Renaming PageIdSteamAppId -> PageId

--- pcgw_integration/page_id.go
+++ pcgw_integration/page_id.go
@@ -1,6 +1,6 @@
 package pcgw_integration
 
-type PageIdSteamAppId struct {
+type PageId struct {
 	Cargoquery []struct {
 		Title struct {
 			PageID string `json:"PageID"`
@@ -12,7 +12,7 @@ type PageIdGetter interface {
 	GetPageId() string
 }
 
-func (ps *PageIdSteamAppId) GetPageId() string {
+func (ps *PageId) GetPageId() string {
 	for _, cq := range ps.Cargoquery {
 		return cq.Title.PageID
 	}
