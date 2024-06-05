c3bb7643a06f947dc80d829ad8e5e318bf508bce
Bogdan Brinza <Bogdan.Brinza@hey.com>
2024-06-04 21:59:43 -0700

    Adding PublishDate, UpdatedAt helpers

--- ign_integration/wiki_props.go
+++ ign_integration/wiki_props.go
@@ -279,6 +279,14 @@ func (wp *WikiProps) PageTitle() string {
 	return wp.Props.PageProps.Page.Page.Title
 }
 
+func (wp *WikiProps) PublishDate() time.Time {
+	return wp.Props.PageProps.Page.PublishDate
+}
+
+func (wp *WikiProps) UpdatedAt() time.Time {
+	return wp.Props.PageProps.Page.UpdatedAt
+}
+
 func (wp *WikiProps) PrimaryImageUrl() string {
 	page := wp.Props.PageProps.Page
 	if page.PrimaryObject.PrimaryImage.Url != "" {
