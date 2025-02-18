d7cde8218f50f8e4c450e75144ea577786a59cf5
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-02-17 17:40:16 -0800

    Adding Steam AppDetails properties methods

--- steam_integration/appdetails.go
+++ steam_integration/appdetails.go
@@ -1,26 +1,28 @@
 package steam_integration
 
+import "strconv"
+
 type AppDetails struct {
-	Type                string       `json:"type"`
-	Name                string       `json:"name"`
-	SteamAppId          uint32       `json:"steam_appid"`
-	RequiredAge         string       `json:"required_age"`
-	IsFree              bool         `json:"is_free"`
-	ControllerSupport   string       `json:"controller_support"`
-	DetailedDescription string       `json:"detailed_description"`
-	AboutTheGame        string       `json:"about_the_game"`
-	ShortDescription    string       `json:"short_description"`
-	SupportedLanguages  string       `json:"supported_languages"`
-	HeaderImage         string       `json:"header_image"`
-	CapsuleImage        string       `json:"capsule_image"`
-	CapsuleImageV5      string       `json:"capsule_imagev5"`
-	Website             string       `json:"website"`
-	PcRequirements      Requirements `json:"pc_requirements"`
-	MacRequirements     Requirements `json:"mac_requirements"`
-	LinuxRequirements   Requirements `json:"linux_requirements"`
-	LegalNotice         string       `json:"legal_notice"`
-	Developers          []string     `json:"developers"`
-	Publishers          []string     `json:"publishers"`
+	Type                string   `json:"type"`
+	Name                string   `json:"name"`
+	SteamAppId          uint32   `json:"steam_appid"`
+	RequiredAge         any      `json:"required_age"` // could be "number" or number
+	IsFree              bool     `json:"is_free"`
+	ControllerSupport   string   `json:"controller_support"`
+	DetailedDescription string   `json:"detailed_description"`
+	AboutTheGame        string   `json:"about_the_game"`
+	ShortDescription    string   `json:"short_description"`
+	SupportedLanguages  string   `json:"supported_languages"`
+	HeaderImage         string   `json:"header_image"`
+	CapsuleImage        string   `json:"capsule_image"`
+	CapsuleImageV5      string   `json:"capsule_imagev5"`
+	Website             string   `json:"website"`
+	PcRequirements      any      `json:"pc_requirements"`    // could be {Requirements} or []
+	MacRequirements     any      `json:"mac_requirements"`   // could be {Requirements} or []
+	LinuxRequirements   any      `json:"linux_requirements"` // could be {Requirements} or []
+	LegalNotice         string   `json:"legal_notice"`
+	Developers          []string `json:"developers"`
+	Publishers          []string `json:"publishers"`
 	PriceOverview       struct {
 		Currency         string `json:"currency"`
 		Initial          int    `json:"initial"`
@@ -36,7 +38,7 @@ type AppDetails struct {
 		Description             string `json:"description"`
 		SelectionText           string `json:"selection_text"`
 		SaveText                string `json:"save_text"`
-		DisplayType             int    `json:"display_type"`
+		DisplayType             any    `json:"display_type"` // could be "number" or number
 		IsRecurringSubscription string `json:"is_recurring_subscription"`
 		Subs                    []struct {
 			PackageId                int    `json:"packageid"`
@@ -112,7 +114,87 @@ type Requirements struct {
 	Recommended string `json:"recommended"`
 }
 
-type AppDetailsResponse struct {
+type AppDetailsSuccessData struct {
 	Success bool       `json:"success"`
 	Data    AppDetails `json:"data"`
+}
+
+type AppDetailsResponse map[string]AppDetailsSuccessData
+
+func (adr AppDetailsResponse) GetAppDetails() *AppDetails {
+	for _, adsd := range adr {
+		return &adsd.Data
+	}
+	return nil
+}
+
+func (ad *AppDetails) GetType() string {
+	return ad.Type
+}
+
+func (ad *AppDetails) GetName() string {
+	return ad.Name
+}
+
+func (ad *AppDetails) GetRequiredAge() int {
+	ra := ad.RequiredAge
+
+	if ira, ok := ra.(int); ok {
+		return ira
+	} else if fra, yeah := ra.(float64); yeah {
+		return int(fra)
+	} else if sra, sure := ra.(string); sure {
+		if ira64, err := strconv.ParseInt(sra, 10, 32); err == nil {
+			return int(ira64)
+		}
+	}
+	return 0
+}
+
+func (ad *AppDetails) GetIsFree() bool {
+	return ad.IsFree
+}
+
+func (ad *AppDetails) GetControllerSupport() string {
+	return ad.ControllerSupport
+}
+
+func (ad *AppDetails) GetShortDescription() string {
+	return ad.ShortDescription
+}
+
+func (ad *AppDetails) GetWebsite() string {
+	return ad.Website
+}
+
+func (ad *AppDetails) GetMetacriticScore() int {
+	return ad.Metacritic.Score
+}
+
+func (ad *AppDetails) GetMetacriticUrl() string {
+	return ad.Metacritic.Url
+}
+
+func (ad *AppDetails) GetCategories() []string {
+	categories := make([]string, 0)
+	for _, category := range ad.Categories {
+		categories = append(categories, category.Description)
+	}
+	return categories
+}
+
+func (ad *AppDetails) GetGenres() []string {
+	genres := make([]string, 0)
+	for _, genre := range ad.Genres {
+		genres = append(genres, genre.Description)
+	}
+	return genres
+}
+
+func (ad *AppDetails) GetSupportUrl() string {
+	return ad.SupportInfo.Url
+}
+
+func (ad *AppDetails) GetSupportEmail() string {
+	return ad.SupportInfo.Email
 }
