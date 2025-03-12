574f19cfc70eda7e8ff3084a07ca7d8b8614a56c
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-03-12 13:21:30 -0700

    Adding Steam AppId extractor from PCGW GOG Id pages, using the first provided Steam AppId for that (PCGW packs all related products Steam AppIds into that property)

--- pcgw_integration/page_id.go
+++ pcgw_integration/page_id.go
@@ -1,9 +1,12 @@
 package pcgw_integration
 
+import "strings"
+
 type PageId struct {
 	Cargoquery []struct {
 		Title struct {
-			PageID string `json:"PageID"`
+			PageID     string `json:"PageID"`
+			SteamAppID string `json:"Steam AppID"`
 		} `json:"title"`
 	} `json:"cargoquery"`
 }
@@ -15,6 +18,16 @@ type PageIdGetter interface {
 func (ps *PageId) GetPageId() string {
 	for _, cq := range ps.Cargoquery {
 		return cq.Title.PageID
+	}
+	return ""
+}
+
+func (ps *PageId) GetSteamAppId() string {
+	for _, cq := range ps.Cargoquery {
+		saids := strings.Split(cq.Title.SteamAppID, ",")
+		for _, said := range saids {
+			return strings.TrimSpace(said)
+		}
 	}
 	return ""
 }
