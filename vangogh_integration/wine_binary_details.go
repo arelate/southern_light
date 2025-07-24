b143fdd5f47c36a8d722d25c353e9fffd11e57c0
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-07-23 19:43:36 -0700

    Adding digest to WineBinaryDetails

--- vangogh_integration/wine_binary_details.go
+++ vangogh_integration/wine_binary_details.go
@@ -4,5 +4,6 @@ type WineBinaryDetails struct {
 	Title    string          `json:"title"`
 	OS       OperatingSystem `json:"os"`
 	Version  string          `json:"version"`
+	Digest   string          `json:"digest,omitempty"`
 	Filename string          `json:"filename"`
 }
