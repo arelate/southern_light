f8b15f95b33615300aa2d360e4ff161413dc227e
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-07-23 19:47:23 -0700

    Using GitHub digest prefix format for HTTP binary sources

--- vangogh_integration/os_wine_binaries.go
+++ vangogh_integration/os_wine_binaries.go
@@ -36,7 +36,7 @@ var OsWineBinaries = map[OperatingSystem][]Binary{
 			Title:   CrossOver,
 			Version: "25.0.1",
 			// Digest source: https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-25.0.1.zip.sha256
-			Digest:      "cf980ff0b2774eec403ed7395509a15913a1450391da0fd3ba4bee6f1ba477e3",
+			Digest:      "sha256:cf980ff0b2774eec403ed7395509a15913a1450391da0fd3ba4bee6f1ba477e3",
 			DownloadUrl: "https://media.codeweavers.com/pub/crossover/cxmac/demo/crossover-25.0.1.zip",
 		},
 		{
@@ -54,14 +54,14 @@ var OsWineBinaries = map[OperatingSystem][]Binary{
 			Version: "v14.44.35211.0",
 			// Digest source: https://download.visualstudio.microsoft.com/download/pr/7ebf5fdb-36dc-4145-b0a0-90d3d5990a61/CC0FF0EB1DC3F5188AE6300FAEF32BF5BEEBA4BDD6E8E445A9184072096B713B/VC_redist.x64.exe
 			// Note the URL path part before the filename
-			Digest:      "cc0ff0eb1dc3f5188ae6300faef32bf5beeba4bdd6e8e445a9184072096b713b",
+			Digest:      "sha256:cc0ff0eb1dc3f5188ae6300faef32bf5beeba4bdd6e8e445a9184072096b713b",
 			DownloadUrl: "https://aka.ms/vs/17/release/vc_redist.x64.exe",
 		},
 		{
 			Title:   VcRedistX86,
 			Version: "v14.44.35211.0",
 			// Digest source: https://download.visualstudio.microsoft.com/download/pr/7ebf5fdb-36dc-4145-b0a0-90d3d5990a61/0C09F2611660441084CE0DF425C51C11E147E6447963C3690F97E0B25C55ED64/VC_redist.x86.exe
-			Digest:      "0c09f2611660441084ce0df425c51c11e147e6447963c3690f97e0b25c55ed64",
+			Digest:      "sha256:0c09f2611660441084ce0df425c51c11e147e6447963c3690f97e0b25c55ed64",
 			DownloadUrl: "https://aka.ms/vs/17/release/vc_redist.x86.exe",
 		},
 	},
