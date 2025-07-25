7fab7c8f33c778ff6eb75b60c2dda115cd0f3d53
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-07-24 20:39:47 -0700

    Adding Binary helpers to get DownloadUrl, Version and Digest

--- vangogh_integration/binary.go
+++ vangogh_integration/binary.go
@@ -1,5 +1,10 @@
 package vangogh_integration
 
+import (
+	"github.com/arelate/southern_light/github_integration"
+	"github.com/boggydigital/kevlar"
+)
+
 type Binary struct {
 	Title           string
 	Version         string
@@ -16,4 +21,56 @@ func (bin *Binary) String() string {
 	default:
 		return bin.Title
 	}
+}
+
+func (bin *Binary) GetDownloadUrl(kvGitHubReleases kevlar.KeyValues) (string, error) {
+	binaryUrl := bin.DownloadUrl
+
+	if binaryUrl != "" {
+		return binaryUrl, nil
+	}
+
+	latestRelease, err := github_integration.GetLatestRelease(bin.GitHubOwnerRepo, kvGitHubReleases)
+	if err != nil {
+		return "", err
+	}
+
+	latestAsset := github_integration.GetReleaseAsset(latestRelease, bin.GitHubAssetGlob)
+
+	return latestAsset.BrowserDownloadUrl, nil
+}
+
+func (bin *Binary) GetVersion(kvGitHubReleases kevlar.KeyValues) (string, error) {
+	binaryVersion := bin.Version
+	if binaryVersion != "" {
+		return binaryVersion, nil
+	}
+
+	latestRelease, err := github_integration.GetLatestRelease(bin.GitHubOwnerRepo, kvGitHubReleases)
+	if err != nil {
+		return "", err
+	}
+
+	return latestRelease.TagName, nil
+}
+
+func (bin *Binary) GetDigest(kvGitHubReleases kevlar.KeyValues) (string, error) {
+	digest := bin.Digest
+
+	if digest != "" {
+		return digest, nil
+	}
+
+	latestRelease, err := github_integration.GetLatestRelease(bin.GitHubOwnerRepo, kvGitHubReleases)
+	if err != nil {
+		return "", err
+	}
+
+	latestAsset := github_integration.GetReleaseAsset(latestRelease, bin.GitHubAssetGlob)
+
+	if latestAsset.Digest != nil {
+		return *latestAsset.Digest, nil
+	}
+
+	return "", nil
 }
