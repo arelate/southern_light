da0c0490ad4dd7dfd41a44687331a525023d8e3d
Bogdan Brinza <Bogdan.Brinza@hey.com>
2025-08-05 21:23:12 -0700

    Fixing steam-app-list AppId type

--- steam_integration/app_list.go
+++ steam_integration/app_list.go
@@ -3,7 +3,7 @@ package steam_integration
 type GetAppListV2Response struct {
 	AppList struct {
 		Apps []struct {
-			AppId int    `json:"appid"`
+			AppId uint32 `json:"appid"`
 			Name  string `json:"name"`
 		} `json:"apps"`
 	} `json:"applist"`
