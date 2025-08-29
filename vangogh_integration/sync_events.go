9aba5c6c34bea0f7853d3c49e245de5b5daf3c20
Bogdan Brinza <bbrinza@amazon.com>
2025-08-29 14:51:19 -0700

    Adding detailed sync events

--- vangogh_integration/sync_events.go
+++ vangogh_integration/sync_events.go
@@ -1,11 +1,43 @@
 package vangogh_integration
 
 const (
-	SyncStartKey    = "sync-start"
-	SyncCompleteKey = "sync-complete"
+	SyncStartKey             = "sync-start"
+	SyncDataKey              = "sync-data"
+	SyncDescriptionImagesKey = "sync-description-images"
+	SyncImagesKey            = "sync-images"
+	SyncDehydrateKey         = "sync-dehydrate"
+	SyncVideoMetadataKey     = "sync-video-metadata"
+	SyncDownloadsKey         = "sync-downloads"
+	SyncCleanupKey           = "sync-cleanup"
+	SyncWineBinaries         = "sync-wine-binaries"
+	SyncBackup               = "sync-backup"
+	SyncCompleteKey          = "sync-complete"
 )
 
+var SyncEventsKeys = []string{
+	SyncStartKey,
+	SyncDataKey,
+	SyncDescriptionImagesKey,
+	SyncImagesKey,
+	SyncDehydrateKey,
+	SyncVideoMetadataKey,
+	SyncDownloadsKey,
+	SyncCleanupKey,
+	SyncWineBinaries,
+	SyncBackup,
+	SyncCompleteKey,
+}
+
 var SyncEventsTitles = map[string]string{
-	SyncStartKey:    "Sync Started",
-	SyncCompleteKey: "Sync Completed",
+	SyncStartKey:             "Started",
+	SyncDataKey:              "Data updated",
+	SyncDescriptionImagesKey: "Description images updated",
+	SyncImagesKey:            "Images updated",
+	SyncDehydrateKey:         "Images dehydrated",
+	SyncVideoMetadataKey:     "Video metadata updated",
+	SyncDownloadsKey:         "Downloads updated",
+	SyncCleanupKey:           "Download cleaned up",
+	SyncWineBinaries:         "WINE binaries updated",
+	SyncBackup:               "Data backed up",
+	SyncCompleteKey:          "Completed",
 }
