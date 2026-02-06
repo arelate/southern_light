739f8f5ddea445cc1eb55ef8136cb47fdbabe2f7
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-02-05 20:37:22 -0800

    Adding sync-generate-missing-checksums event

--- vangogh_integration/sync_events.go
+++ vangogh_integration/sync_events.go
@@ -1,18 +1,19 @@
 package vangogh_integration
 
 const (
-	SyncInterruptedKey       = "sync-interrupted"
-	SyncStartKey             = "sync-start"
-	SyncDataKey              = "sync-data"
-	SyncDescriptionImagesKey = "sync-description-images"
-	SyncImagesKey            = "sync-images"
-	SyncDehydrateKey         = "sync-dehydrate"
-	SyncVideoMetadataKey     = "sync-video-metadata"
-	SyncDownloadsKey         = "sync-downloads"
-	SyncCleanupKey           = "sync-cleanup"
-	SyncBinaries             = "sync-binaries"
-	SyncBackup               = "sync-backup"
-	SyncCompleteKey          = "sync-complete"
+	SyncInterruptedKey           = "sync-interrupted"
+	SyncStartKey                 = "sync-start"
+	SyncDataKey                  = "sync-data"
+	SyncDescriptionImagesKey     = "sync-description-images"
+	SyncImagesKey                = "sync-images"
+	SyncDehydrateKey             = "sync-dehydrate"
+	SyncVideoMetadataKey         = "sync-video-metadata"
+	SyncDownloadsKey             = "sync-downloads"
+	SyncGenerateMissingChecksums = "sync-generate-missing-checksums"
+	SyncCleanupKey               = "sync-cleanup"
+	SyncBinaries                 = "sync-binaries"
+	SyncBackup                   = "sync-backup"
+	SyncCompleteKey              = "sync-complete"
 )
 
 var SyncEventsKeys = []string{
@@ -24,6 +25,7 @@ var SyncEventsKeys = []string{
 	SyncDehydrateKey,
 	SyncVideoMetadataKey,
 	SyncDownloadsKey,
+	SyncGenerateMissingChecksums,
 	SyncCleanupKey,
 	SyncBinaries,
 	SyncBackup,
@@ -31,28 +33,30 @@ var SyncEventsKeys = []string{
 }
 
 var CurrentSyncEventForCompleted = map[string]string{
-	SyncStartKey:             SyncDataKey,
-	SyncDataKey:              SyncDescriptionImagesKey,
-	SyncDescriptionImagesKey: SyncImagesKey,
-	SyncImagesKey:            SyncDehydrateKey,
-	SyncDehydrateKey:         SyncVideoMetadataKey,
-	SyncVideoMetadataKey:     SyncDownloadsKey,
-	SyncDownloadsKey:         SyncCleanupKey,
-	SyncCleanupKey:           SyncBinaries,
-	SyncBinaries:             SyncBackup,
+	SyncStartKey:                 SyncDataKey,
+	SyncDataKey:                  SyncDescriptionImagesKey,
+	SyncDescriptionImagesKey:     SyncImagesKey,
+	SyncImagesKey:                SyncDehydrateKey,
+	SyncDehydrateKey:             SyncVideoMetadataKey,
+	SyncVideoMetadataKey:         SyncDownloadsKey,
+	SyncDownloadsKey:             SyncGenerateMissingChecksums,
+	SyncGenerateMissingChecksums: SyncCleanupKey,
+	SyncCleanupKey:               SyncBinaries,
+	SyncBinaries:                 SyncBackup,
 }
 
 var CurrentSyncEventsTitles = map[string]string{
-	SyncInterruptedKey:       "Interrupted",
-	SyncStartKey:             "Started sync",
-	SyncDataKey:              "Updating data",
-	SyncDescriptionImagesKey: "Updating descriptions",
-	SyncImagesKey:            "Updating images",
-	SyncDehydrateKey:         "Processing images",
-	SyncVideoMetadataKey:     "Updating videos",
-	SyncDownloadsKey:         "Downloading files",
-	SyncCleanupKey:           "Cleaning up downloads",
-	SyncBinaries:             "Updating binaries",
-	SyncBackup:               "Backing up data",
-	SyncCompleteKey:          "Sync complete", // this should be the last key
+	SyncInterruptedKey:           "Interrupted",
+	SyncStartKey:                 "Started sync",
+	SyncDataKey:                  "Updating data",
+	SyncDescriptionImagesKey:     "Updating descriptions",
+	SyncImagesKey:                "Updating images",
+	SyncDehydrateKey:             "Processing images",
+	SyncVideoMetadataKey:         "Updating videos",
+	SyncDownloadsKey:             "Downloading files",
+	SyncGenerateMissingChecksums: "Generating missing checksums",
+	SyncCleanupKey:               "Cleaning up downloads",
+	SyncBinaries:                 "Updating binaries",
+	SyncBackup:                   "Backing up data",
+	SyncCompleteKey:              "Sync complete", // this should be the last key
 }
