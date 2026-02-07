7706b015b92190c42556e8acce844e584ab83533
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-02-06 19:23:41 -0800

    Adding Steam Proton runtimes

--- wine_integration/os_wine_binaries.go
+++ wine_integration/os_wine_binaries.go
@@ -1,6 +1,11 @@
 package wine_integration
 
-import "github.com/arelate/southern_light/vangogh_integration"
+import (
+	"maps"
+	"slices"
+
+	"github.com/arelate/southern_light/vangogh_integration"
+)
 
 const (
 	UmuLauncher        = "Open-Wine-Components/umu-launcher"
@@ -13,6 +18,44 @@ const (
 	VcRedistX64        = "Visual C++ Redistributable X64"
 	VcRedistX86        = "Visual C++ Redistributable X86"
 )
+
+const (
+	SteamProton316                  = "proton-3.16"
+	SteamProton370                  = "proton-3.7"
+	SteamProton411                  = "proton-4.11"
+	SteamProton420                  = "proton-4.2"
+	SteamProton500                  = "proton-5.0"
+	SteamProton513                  = "proton-5.13"
+	SteamProton630                  = "proton-6.3"
+	SteamProton700                  = "proton-7.0"
+	SteamProton800                  = "proton-8.0"
+	SteamProton900                  = "proton-9.0"
+	SteamProton1000                 = "proton-10.0"
+	SteamProtonNext                 = "proton-next"
+	SteamProtonHotfix               = "proton-hotfix"
+	SteamProtonExperimental         = "proton-experimental"
+	SteamProtonBattlEyeRuntime      = "proton-battleye-runtime"
+	SteamProtonEasyAntiCheatRuntime = "proton-easy-anti-cheat-runtime"
+)
+
+var SteamProtonDirectories = map[string]string{
+	SteamProton316:                  "Proton 3.16",
+	SteamProton370:                  "Proton 3.7",
+	SteamProton411:                  "Proton 4.11",
+	SteamProton420:                  "Proton 4.2",
+	SteamProton500:                  "Proton 5.0",
+	SteamProton513:                  "Proton 5.13",
+	SteamProton630:                  "Proton 6.3",
+	SteamProton700:                  "Proton 7.0",
+	SteamProton800:                  "Proton 8.0",
+	SteamProton900:                  "Proton 9.0",
+	SteamProton1000:                 "Proton 10.0",
+	SteamProtonNext:                 "Proton Next",
+	SteamProtonHotfix:               "Proton Hotfix",
+	SteamProtonExperimental:         "Proton Experimental",
+	SteamProtonBattlEyeRuntime:      "Proton BattlEye Runtime",
+	SteamProtonEasyAntiCheatRuntime: "Proton Easy Anti-Cheat Runtime",
+}
 
 const (
 	tarGz = "*.tar.gz"
@@ -77,4 +120,13 @@ var OsWineBinaries = []Binary{
 		DownloadUrl: "https://aka.ms/vs/17/release/vc_redist.x86.exe",
 		OS:          vangogh_integration.Windows,
 	},
+}
+
+func AllSteamProtonRuntimes() []string {
+	return slices.Collect(maps.Keys(SteamProtonDirectories))
+}
+
+func IsSteamProtonRuntime(runtime string) bool {
+	_, ok := SteamProtonDirectories[runtime]
+	return ok
 }
