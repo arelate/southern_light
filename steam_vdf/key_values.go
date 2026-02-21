4a8817016d84a1ce5341f41b2127bd7637abb1fb
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-02-20 21:20:14 -0800

    Removing AppInfo and replacing it with the way to query properties of ValveDataFile

--- steam_vdf/key_values.go
+++ steam_vdf/key_values.go
@@ -11,24 +11,10 @@ type KeyValues struct {
 	Value      *string
 	Type       BinaryType
 	TypedValue any
-	Values     []*KeyValues
+	Values     ValveDataFile
 }
 
 type ValveDataFile []*KeyValues
-
-func (kv *KeyValues) Val(key string) string {
-	for _, value := range kv.Values {
-		if value.Key == key {
-			switch value.Value {
-			case nil:
-				return ""
-			default:
-				return *value.Value
-			}
-		}
-	}
-	return ""
-}
 
 func (vdf ValveDataFile) At(pathParts ...string) (*KeyValues, error) {
 
@@ -50,4 +36,20 @@ func (vdf ValveDataFile) At(pathParts ...string) (*KeyValues, error) {
 	}
 
 	return current, nil
+}
+
+func (vdf ValveDataFile) Val(pathParts ...string) (string, bool) {
+	at, err := vdf.At(pathParts...)
+	if errors.Is(err, ErrVdfKeyNotFound) {
+		return "", false
+	} else if err != nil {
+		return "", false
+	}
+
+	switch at.Value {
+	case nil:
+		return "", false
+	default:
+		return *at.Value, true
+	}
 }
