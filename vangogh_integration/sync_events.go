78ddde3f6a1f532d6b7855e965ab3804b86c256e
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-04-13 19:55:05 -0700

    Updating sync events

--- vangogh_integration/sync_events.go
+++ vangogh_integration/sync_events.go
@@ -1,62 +1,72 @@
 package vangogh_integration
 
 const (
+	SyncUnknownKey               = "sync-unknown"
 	SyncInterruptedKey           = "sync-interrupted"
 	SyncStartKey                 = "sync-start"
 	SyncPurchasesDataKey         = "sync-purchases-data"
-	SyncDescriptionImagesKey     = "sync-description-images"
-	SyncImagesKey                = "sync-images"
-	SyncVideoMetadataKey         = "sync-video-metadata"
+	SyncReducePurchasesDataKey   = "sync-reduce-purchases-data"
+	SyncPurchasesImagesKey       = "sync-purchases-images"
 	SyncDownloadsKey             = "sync-downloads"
 	SyncGenerateMissingChecksums = "sync-generate-missing-checksums"
 	SyncCleanupKey               = "sync-cleanup"
+	SyncVideoMetadataKey         = "sync-video-metadata"
 	SyncBinaries                 = "sync-binaries"
 	SyncExtraData                = "sync-extra-data"
+	SyncReduceExtraDataKey       = "sync-reduce-extra-data"
+	SyncExtraImagesKey           = "sync-extra-images"
+	SyncDescriptionImagesKey     = "sync-description-images"
 	SyncBackup                   = "sync-backup"
 	SyncCompleteKey              = "sync-complete"
 )
 
-var SyncEventsKeys = []string{
+var SyncEventsSequence = []string{
 	SyncInterruptedKey,
 	SyncStartKey,
 	SyncPurchasesDataKey,
-	SyncDescriptionImagesKey,
-	SyncImagesKey,
-	SyncVideoMetadataKey,
+	SyncReducePurchasesDataKey,
+	SyncPurchasesImagesKey,
 	SyncDownloadsKey,
 	SyncGenerateMissingChecksums,
 	SyncCleanupKey,
+	SyncVideoMetadataKey,
 	SyncBinaries,
 	SyncExtraData,
+	SyncReduceExtraDataKey,
+	SyncExtraImagesKey,
+	SyncDescriptionImagesKey,
 	SyncBackup,
 	SyncCompleteKey, // this should be the last key
 }
 
-var CurrentSyncEventForCompleted = map[string]string{
-	SyncStartKey:                 SyncPurchasesDataKey,
-	SyncPurchasesDataKey:         SyncDescriptionImagesKey,
-	SyncDescriptionImagesKey:     SyncImagesKey,
-	SyncImagesKey:                SyncVideoMetadataKey,
-	SyncVideoMetadataKey:         SyncDownloadsKey,
-	SyncDownloadsKey:             SyncGenerateMissingChecksums,
-	SyncGenerateMissingChecksums: SyncCleanupKey,
-	SyncCleanupKey:               SyncBinaries,
-	SyncBinaries:                 SyncExtraData,
-	SyncExtraData:                SyncBackup,
+func NextSyncEvent(completedEvent string) string {
+	for ii, event := range SyncEventsSequence {
+		if event == completedEvent {
+			if ii == 0 || ii == len(SyncEventsSequence)-1 {
+				return SyncUnknownKey
+			}
+			return SyncEventsSequence[ii+1]
+		}
+	}
+	return SyncUnknownKey
 }
 
-var CurrentSyncEventsTitles = map[string]string{
+var SyncEventsTitles = map[string]string{
+	SyncUnknownKey:               "Unknown",
 	SyncInterruptedKey:           "Interrupted",
 	SyncStartKey:                 "Started sync",
 	SyncPurchasesDataKey:         "Updating account data",
-	SyncDescriptionImagesKey:     "Updating descriptions",
-	SyncImagesKey:                "Updating images",
-	SyncVideoMetadataKey:         "Updating videos",
+	SyncReducePurchasesDataKey:   "Reducing account data",
+	SyncPurchasesImagesKey:       "Updating account images",
 	SyncDownloadsKey:             "Downloading files",
 	SyncGenerateMissingChecksums: "Generating missing checksums",
 	SyncCleanupKey:               "Cleaning up downloads",
+	SyncVideoMetadataKey:         "Updating video titles",
 	SyncBinaries:                 "Updating binaries",
 	SyncExtraData:                "Updating extra data",
+	SyncReduceExtraDataKey:       "Reducing extra data",
+	SyncExtraImagesKey:           "Updating extra images",
+	SyncDescriptionImagesKey:     "Updating descriptions",
 	SyncBackup:                   "Backing up data",
 	SyncCompleteKey:              "Sync complete", // this should be the last key
 }
