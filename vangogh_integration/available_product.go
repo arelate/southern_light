70b788dc2cfc9310f4f9e7a3306e1f16b34d83cb
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-04-25 19:21:50 -0700

    Adding DLCs to AvailableProduct

--- vangogh_integration/available_product.go
+++ vangogh_integration/available_product.go
@@ -4,4 +4,5 @@ type AvailableProduct struct {
 	Id               string            `json:"id"`
 	Title            string            `json:"tt"`
 	OperatingSystems []OperatingSystem `json:"os"`
+	Dlc              map[string]string `json:"dlc,omitempty"`
 }
