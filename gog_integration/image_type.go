f49007f86d4c07d9d690471c59965a0273e44b1f
Bogdan Brinza <Bogdan.Brinza@hey.com>
2026-06-22 19:51:21 -0700

    Moving image type leftovers from vangogh_integration to gog_integration

--- gog_integration/image_type.go
+++ gog_integration/image_type.go
@@ -1,7 +1,9 @@
 package gog_integration
 
 import (
+	"errors"
 	"maps"
+	"net/url"
 	"slices"
 )
 
@@ -75,4 +77,56 @@ func ImageTypesCloValues() []string {
 		itsStr = append(itsStr, it.String())
 	}
 	return itsStr
+}
+
+func ImagePropertyExt(it ImageType) (string, error) {
+
+	var ext string
+	var err error
+
+	switch it {
+	case Image:
+		fallthrough
+	case VerticalImage:
+		fallthrough
+	case Screenshots:
+		fallthrough
+	case Hero:
+		fallthrough
+	case Background:
+		ext = JpgExt
+	case Icon:
+		fallthrough
+	case IconSquare:
+		fallthrough
+	case Logo:
+		ext = PngExt
+	default:
+		err = errors.New("no url for unknown image type")
+	}
+
+	return ext, err
+}
+
+func ImagePropertyUrls(imageIds []string, it ImageType) ([]*url.URL, error) {
+	urls := make([]*url.URL, 0, len(imageIds))
+
+	ext, err := ImagePropertyExt(it)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, imageId := range imageIds {
+		if imageId == "" {
+			continue
+		}
+		var imageUrl *url.URL
+		imageUrl, err = ImageUrl(imageId, ext)
+		if err != nil {
+			return urls, err
+		}
+		urls = append(urls, imageUrl)
+	}
+
+	return urls, nil
 }
